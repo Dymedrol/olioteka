f393a5159c33a13b002627822e636bc1b8cdbda3
Dymedrol <dmitrykob@gmail.com>
2025-02-11 17:31:15 +0700

    header update

--- source/js/script.js
+++ source/js/script.js
@@ -1,97 +1,19 @@
 // import { initWishList } from './wishlist';
-// import { initCheckUpload } from './checkUpload';
-// import { initProductPage } from './productPage';
-// import { initCollectionPage } from './collection-page';
-// import { initCart } from './cart';
-// import { initMobileMenuAccordion } from './mobileMenuAccordion';
-// import { initSearch } from './search';
 
-// $(document).ready(function(){
-//   initWishList();
-//   // initCheckUpload();
-//   initProductPage();
-//   initCart();
-//   initCollectionPage();
-//   initMobileMenuAccordion();
-//   initSearch();
+$(document).ready(function(){
+  // initWishList();
+  const burger = $('#burger');
+  const mobileMenu = $('#mobile-menu');
+  const mobileMenuClose = mobileMenu.find('.olio-header-menu-mobile__close');
 
-//   var checkDummyItems = function () {
-//     var items = $('.special-product-wall__item-dummy');
+  burger.click(function() {
+    console.log('!!!!', mobileMenu.length)
+    mobileMenu.show("fast");
+  });
 
-//     if (!items.length) {
-//       return
-//     }
-
-//     items.each( function() {
-//       if ($(this).height() > 20 ) {
-//         $(this).removeClass('special-list__item-dummy_no-border');
-//       } else {
-//         $(this).addClass('special-list__item-dummy_no-border');
-//       }
-//     });
-//   }
-
-//   setInterval(checkDummyItems, 1000);
-
-
-//   $(".special-categories-mobile").owlCarousel({
-//     margin: 1,
-//     loop: true,
-//     autoWidth: true,
-//   });
-
-//   $('.special-list-mobile').owlCarousel({
-//     margin: 1,
-//     loop: true,
-//     autoWidth: true,
-//   });
-
-//   $('.special-pdp-slider').slick({
-//     slidesToShow: 1,
-//     infinite: true,
-//     swipeToSlide: true,
-//   });
-
-//   var menu =  $('.special-product-controls__menu');
-
-//   $('#plus').click(function() {
-//     $('.special-menu__mobile').show();
-//     $('body').addClass('no-scroll');
-//   });
-
-//   $('.special-menu__mobile-close').click(function(){
-//     $('.special-menu__mobile').hide();
-//     $('body').removeClass('no-scroll');
-//   });
-
-
-
-//   $('.special-product-controls__filters').click(function() {
-//     menu.animate({width:'toggle'},250);
-//     $('body').toggleClass('no-scroll');
-//   });
-
-//   $('.special-product-controls__menu-close').click(function() {
-//     menu.animate({width:'toggle'},250);
-//     $('body').toggleClass('no-scroll');
-//   });
-
-//   var bagInner = $('.special-bag-warnings');
-
-//   if (bagInner.length) {
-
-//     var minHeight = $('#MainContent').height();
-//     bagInner.css('min-height', minHeight);
-
-//     $( window ).resize(function() {
-//       minHeight = $('#MainContent').height();
-//       bagInner.css('min-height', minHeight);
-//     });
-
-//   }
-
-
-
-// })
+  mobileMenuClose.click(function() {
+    mobileMenu.hide('fast');
+  });
+})
 
 
