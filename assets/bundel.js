659dc245d20b4a168a5de8ac488495aec07c9cb2
Dymedrol <dmitrykob@gmail.com>
2025-02-17 14:08:27 +0700

    Produxts thumbneils update

--- assets/bundel.js
+++ assets/bundel.js
@@ -103,9 +103,9 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sou
   !*** ./node_modules/mini-css-extract-plugin/dist/loader.js!./node_modules/css-loader/dist/cjs.js??ref--5-2!./node_modules/less-loader/dist/cjs.js!./source/less/index.less ***!
   \*****************************************************************************************************************************************************************************/
 /*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
-eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./source/less/index.less?./node_modules/mini-css-extract-plugin/dist/loader.js!./node_modules/css-loader/dist/cjs.js??ref--5-2!./node_modules/less-loader/dist/cjs.js");
+eval("throw new Error(\"Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\\nModuleBuildError: Module build failed (from ./node_modules/less-loader/dist/cjs.js):\\n\\r\\n\\r\\n\\r\\n  & :focus-visible] {\\r\\n                ^\\r\\nUnrecognised input\\r\\n      in C:\\\\work\\\\shopify\\\\olioteka\\\\source\\\\less\\\\blocks\\\\product.less (line 109, column 18)\\n    at C:\\\\work\\\\shopify\\\\olioteka\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:316:20\\n    at C:\\\\work\\\\shopify\\\\olioteka\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:367:11\\n    at C:\\\\work\\\\shopify\\\\olioteka\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:233:18\\n    at context.callback (C:\\\\work\\\\shopify\\\\olioteka\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:111:13)\");\n\n//# sourceURL=webpack:///./source/less/index.less?./node_modules/mini-css-extract-plugin/dist/loader.js!./node_modules/css-loader/dist/cjs.js??ref--5-2!./node_modules/less-loader/dist/cjs.js");
 
 /***/ }),
 
